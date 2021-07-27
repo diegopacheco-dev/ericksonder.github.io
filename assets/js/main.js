190c1d21fbf14dc110f2a5f46290c337a55418be
ericksonder <ericksonder017@gmail.com>
2021-07-27 04:04:27 -0500

    Agregue efectos gsap al home

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,6 +62,11 @@ const scrollHeader = () => {
 window.addEventListener('scroll', scrollHeader);
 
 
+/*===== GSAP ANIMATION =====*/ 
+gsap.from('.home__data', {opacity: 0, duration: 2, delay:.8, y: 25});
+gsap.from('.home__greeting, .home__name, .home__profession, .home__button', {opacity: 0, duration: 2, delay:1, y: 25, ease:'expo.out', stagger:.2});
+gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 1.5, delay:1, y: 25, ease:'expo.out', stagger:.2});
+gsap.from('.nav__item', {opacity: 0, duration: 1.8, delay:1, y: 25, ease:'expo.out', stagger:.2});
 
 
 
